178c85ad0e080cd8661806b8b8cbb7d8de482ac1
Cody Mickelsen <63523816+cody-mick@users.noreply.github.com>
2023-01-19 12:00:47 -0700

    👌 IMPROVE: Begin work on input component

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Input({ inputType }: any) {
 	return (
 		<div className="input-container">
+			<label>Enter Food</label>
 			<input className="input" type={inputType} />
 		</div>
 	);
